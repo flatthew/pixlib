b26f803f77334600f219453c9cf1647ae6b4b365
Your Name <artofbeingmjeff@gmail.com>
2022-04-30 15:15:59 +0100

    restructured to actually be a library oml what was i thinking

--- legacySubroutines.py
+++
@@ -1,7 +0,0 @@
-def legacyUpdatePixel(canvas,pixel,acolour):
-    pixel.image.fill(color=acolour)
-    if len(acolour) == 4:
-        pixel.r,pixel.g,pixel.b,pixel.a = acolour
-    else:
-        pixel.r,pixel.g,pixel.b = acolour
-    canvas.blit(pixel.image,pixel.Rect)
