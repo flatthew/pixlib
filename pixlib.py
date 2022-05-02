53d4e5320185fe84b07b4c451c2242a2f7ce538b
Your Name <artofbeingmjeff@gmail.com>
2022-05-02 15:40:12 +0100

    used class inheritance to improve state functionality

--- pixlib.py
+++ pixlib.py
@@ -19,32 +19,10 @@ class pixel(pg.sprite.Sprite):
             self.r,self.g,self.b = acolour
         canvas.blit(self.image,self.Rect)
 
-class pixlib():
-    def __init__(self,windowWidth,windowHeight,pixelsize):
-        self.windowDim = [windowWidth,windowHeight]
-        self.pixarrayDim = [windowWidth//pixelsize,windowHeight//pixelsize]
-        self.pixelsize = pixelsize
-        self.pixels = self.__initPixelarray__()
-        self.window = pg.display.set_mode((windowWidth, windowHeight))
-        self.pixelsize = pixelsize
-        self.visualStates = {}
-        self.currentState = ''
-        self.x = 0
-        self.y = 0
-
-    def __initPixelarray__(self):
-        pixels = []
-        for i in range(0,self.windowDim[0],self.pixelsize):
-            column = []
-            for x in range(0,self.windowDim[1],self.pixelsize):
-                column.append(pixel(i,x,self.pixelsize,(0,0,0,255)))
-            pixels.append(column)
-        return pixels
-
-    def drawBasicBG(self,colour):
-        for i in range(0,self.pixarrayDim[0]):
-            for x in range(0,self.pixarrayDim[1]):
-                self.pixels[i][x].updatePixel(self.window,colour)
+class states_root():
+    def __init__(self,states) -> None:
+        self.visualStates = states
+        self.currentState = states['main']
 
     def addState(self,statename,statearray):
         if self.visualStates.get(statename,-1) != -1:
@@ -72,7 +50,7 @@ class pixlib():
         else:
             self.visualStates[statename] = newStateVal
 
-    def drawStateBG(self):
+    def draw(self):
         for a in range(len(self.visualStates[self.currentState])):
             for b in range(len(self.visualStates[self.currentState][a])):
                 linepos = 0
@@ -82,39 +60,40 @@ class pixlib():
                     for c in range(self.visualStates[self.currentState][a][b][0]):
                         self.pixels[self.x+linepos][self.y+a].updatePixel(self.window,self.visualStates[self.currentState][a][b][1])
 
-class pix_sprite():
-    def __init__(self,aposition,aheight,awidth,**kwargs):
+class pix_canvas(states_root):
+    def __init__(self,windowWidth,windowHeight,pixelsize,states):
+        self.windowDim = [windowWidth,windowHeight]
+        self.pixarrayDim = [windowWidth//pixelsize,windowHeight//pixelsize]
+        self.pixelsize = pixelsize
+        self.pixels = self.__initPixelarray__()
+        self.window = pg.display.set_mode((windowWidth, windowHeight))
+        self.pixelsize = pixelsize
+        super().__init__(states)
+        self.currentState = ''
+        self.x = 0
+        self.y = 0
+
+    def __initPixelarray__(self):
+        pixels = []
+        for i in range(0,self.windowDim[0],self.pixelsize):
+            column = []
+            for x in range(0,self.windowDim[1],self.pixelsize):
+                column.append(pixel(i,x,self.pixelsize,(0,0,0,255)))
+            pixels.append(column)
+        return pixels
+
+    def drawBasicBG(self,colour):
+        for i in range(0,self.pixarrayDim[0]):
+            for x in range(0,self.pixarrayDim[1]):
+                self.pixels[i][x].updatePixel(self.window,colour)
+
+class pix_sprite(states_root):
+    def __init__(self,aposition,aheight,awidth,states):
         self.x, self.y = aposition
         self.height = aheight
         self.width = awidth
-        self.visualStates = kwargs
+        super().__init__(states)
         self.currentState = ''
-
-    def addState(self,statename,statearray):
-        if self.visualStates.get(statename,-1) != -1:
-            raise ValueError('State ' + statename + ' already exists.')
-        else:
-            self.visualStates[statename] = statearray
-    
-    def removeState(self,statename):
-        if self.visualStates.get(statename,-1) == -1:
-            raise ValueError('There is no state called \''+ statename +'\' and therefore it cannot be removed.')
-        else:
-            self.visualStates.pop(statename)
-
-    def switchState(self,statename):
-        if self.visualStates.get(statename,-1) == -1:
-            raise ValueError('No such state as ' + statename)
-        else:
-            self.currentState = statename
-
-    def alterState(self,statename,newStateVal):
-        if self.visualStates.get(statename,-1) == -1:
-            raise ValueError('No such state as ' + statename)
-        elif type(newStateVal) != list and type(newStateVal) != tuple:
-            raise ValueError('State in invalid format')
-        else:
-            self.visualStates[statename] = newStateVal
 
     def updatePos(self,screen,pixels,aposition):
         self.x, self.y = aposition
